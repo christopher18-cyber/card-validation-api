7d726e93a197504e839a8b1d9658f4d33f64e39a
christopher18-cyber <christopheradegoke4@gmail.com>
2026-09-03 03:07:29 +0100

    feat: implemented the logger for easy debugging

---
+++ utils/logger.ts
@@ -0,0 +1,29 @@
+import "dotenv/config"
+import winston from "winston";
+const logger = winston.createLogger({
+	level: process.env.NODE_ENV === "production" ? "info" : "debug",
+	format: winston.format.combine(
+		winston.format.timestamp(),
+		winston.format.json(),
+		winston.format.errors({ stacks: true }),
+		winston.format.splat(),
+	),
+	defaultMeta: { service: "CARD_VALIDATION_API" },
+	transports: [
+		new winston.transports.Console({
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple(),
+			),
+		}),
+		new winston.transports.File({
+			filename: "error.log",
+			level: "error",
+		}),
+		new winston.transports.File({
+			filename: "combined.log",
+		}),
+	],
+});
+
+export default logger;
